e5d0ab86cbd08ca0b7f5628409d0bb0bacb00519
ilangoselvan <ilangolit@gmail.com>
2023-03-27 00:26:03 +0530

    new file add

---
+++ main.js
@@ -0,0 +1,13 @@
+//Display Current Time of your Machine
+//current time so getlocaletimestring
+
+let current=new Date();
+console.log(current);
+console.log(current.getDate());
+console.log(current.getDay());
+console.log(current.toLocaleTimeString());
+
+
+
+
+
